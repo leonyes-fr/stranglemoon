efc66ac0162b165b747d83014b8707493d8f0cb2
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-08 14:58:18 +0200

    premier commit

---
+++ src/main/java/com/stranglemoon/stranglemoon/StranglemoonApplication.java
@@ -0,0 +1,13 @@
+package com.stranglemoon.stranglemoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StranglemoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StranglemoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stranglemoon/stranglemoon/StranglemoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stranglemoon.stranglemoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StranglemoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
