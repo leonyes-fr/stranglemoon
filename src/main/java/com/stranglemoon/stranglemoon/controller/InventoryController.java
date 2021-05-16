0947d0f13f3ece0e6ca6af84c885934bf3890e6e
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-16 15:45:34 +0200

    put fonctionne sur gold carrot.

--- src/main/java/com/stranglemoon/stranglemoon/controller/InventoryController.java
+++ src/main/java/com/stranglemoon/stranglemoon/controller/InventoryController.java
@@ -1,19 +1,40 @@
 package com.stranglemoon.stranglemoon.controller;
 
 import com.stranglemoon.stranglemoon.model.Inventory;
+import com.stranglemoon.stranglemoon.repository.InventoryRepository;
 import com.stranglemoon.stranglemoon.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class InventoryController {
 
     @Autowired
     private InventoryService inventoryService;
 
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
     @GetMapping("/inventories")
     public Iterable<Inventory> getInventories() {
         return inventoryService.getInventories();
     }
+
+
+    @PutMapping(value = "/inventories/{id}")
+    public @ResponseBody Inventory updateInventory(@PathVariable("id") long id) {
+        Optional<Inventory> optionalInventory = inventoryRepository.findById(id);
+        Inventory inventory = optionalInventory.get();
+        inventory.setGold(inventory.getGold() + 10);
+        inventory.setCarrot(inventory.getCarrot() - 1);
+
+        inventoryRepository.save(inventory);
+        return inventory;
+    }
+
+
+
 }
