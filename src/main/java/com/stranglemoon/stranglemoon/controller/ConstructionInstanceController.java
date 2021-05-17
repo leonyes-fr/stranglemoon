45d241d491c12094cd20555b294443ef8554c4ae
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-17 22:01:55 +0200

    controller constructionCost pour update tavern rank.

--- src/main/java/com/stranglemoon/stranglemoon/controller/ConstructionInstanceController.java
+++ src/main/java/com/stranglemoon/stranglemoon/controller/ConstructionInstanceController.java
@@ -1,11 +1,13 @@
 package com.stranglemoon.stranglemoon.controller;
 
 import com.stranglemoon.stranglemoon.model.ConstructionInstance;
+import com.stranglemoon.stranglemoon.model.Inventory;
+import com.stranglemoon.stranglemoon.repository.ConstructionInstanceRepository;
 import com.stranglemoon.stranglemoon.service.ConstructionInstanceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -14,10 +16,24 @@ public class ConstructionInstanceController {
     @Autowired
     private ConstructionInstanceService constructionInstanceService;
 
+    @Autowired
+    private ConstructionInstanceRepository constructionInstanceRepository;
+
     @GetMapping("/constructioninstances")
     public Iterable<ConstructionInstance> getConstructionInstances() {
         return constructionInstanceService.getConstructionInstances();
 
+    }
+
+    @PutMapping(value = "/constructioninstance/nextrank/{id}")
+    public @ResponseBody
+    ConstructionInstance updateTavern(@PathVariable("id") long id) {
+        Optional<ConstructionInstance> optionalConstructionInstance = constructionInstanceRepository.findById(id);
+        ConstructionInstance constructionInstance = optionalConstructionInstance.get();
+        constructionInstance.setActualRank(constructionInstance.getActualRank() + 1);
+
+        constructionInstanceRepository.save(constructionInstance);
+        return constructionInstance;
     }
 
 }
