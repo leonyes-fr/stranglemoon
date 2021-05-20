dfe69e5720b3f2bfa236d6e4d17825ee313d6938
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-20 21:24:08 +0200

    logique métier placé dans le service.

--- src/main/java/com/stranglemoon/stranglemoon/controller/ConstructionInstanceController.java
+++ src/main/java/com/stranglemoon/stranglemoon/controller/ConstructionInstanceController.java
@@ -1,15 +1,9 @@
 package com.stranglemoon.stranglemoon.controller;
 
-import com.stranglemoon.stranglemoon.model.ConstructionCost;
 import com.stranglemoon.stranglemoon.model.ConstructionInstance;
-import com.stranglemoon.stranglemoon.model.Inventory;
-import com.stranglemoon.stranglemoon.service.ConstructionCostService;
 import com.stranglemoon.stranglemoon.service.ConstructionInstanceService;
-import com.stranglemoon.stranglemoon.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -18,11 +12,7 @@ public class ConstructionInstanceController {
     @Autowired
     private ConstructionInstanceService constructionInstanceService;
 
-    @Autowired
-    private InventoryService inventoryService;
 
-    @Autowired
-    private ConstructionCostService constructionCostService;
 
     @GetMapping("/constructioninstances")
     public Iterable<ConstructionInstance> getConstructionInstances() {
@@ -33,24 +23,7 @@ public class ConstructionInstanceController {
     @PutMapping(value = "/constructioninstance/nextrank/{id}")
     public @ResponseBody
     ConstructionInstance updateTavern(@PathVariable("id") long id) {
-        Optional<ConstructionInstance> optionalConstructionInstance = constructionInstanceService.getConstructionInstance(id);
-        ConstructionInstance constructionInstance = optionalConstructionInstance.get();  // j'ai mon instance de taverne avec son actual Rank
-
-        Optional<ConstructionCost> optionalConstructionCost = constructionCostService.getConstructionCost(new Long(constructionInstance.getActualRank()));
-        ConstructionCost constructionCost = optionalConstructionCost.get(); // Je prend le cout de construction d'une taverne ou l'id est = a actual Rank de mon instance.
-
-        Inventory inventory = inventoryService.getInventories().iterator().next();  //je prend mon inventaire perso.
-
-        if (inventory.getGold() - constructionCost.getTavern() > 0) {  // Si 100 - 20 > 0 alors...
-            inventory.setGold(inventory.getGold() - constructionCost.getTavern());
-            constructionInstance.setActualRank(constructionInstance.getActualRank() + 1);
-            constructionInstanceService.saveConstructionInstance(constructionInstance);
-            return constructionInstance;
-        }else {
-            return constructionInstance;
-        }
-
-
+        return constructionInstanceService.updateTavern(id);
     }
 
 }
--- src/main/java/com/stranglemoon/stranglemoon/service/ConstructionInstanceService.java
+++ src/main/java/com/stranglemoon/stranglemoon/service/ConstructionInstanceService.java
@@ -1,6 +1,8 @@
 package com.stranglemoon.stranglemoon.service;
 
+import com.stranglemoon.stranglemoon.model.ConstructionCost;
 import com.stranglemoon.stranglemoon.model.ConstructionInstance;
+import com.stranglemoon.stranglemoon.model.Inventory;
 import com.stranglemoon.stranglemoon.repository.ConstructionInstanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,12 @@ public class ConstructionInstanceService {
     @Autowired
     private ConstructionInstanceRepository constructionInstanceRepository;
 
+    @Autowired
+    private InventoryService inventoryService;
+
+    @Autowired
+    private ConstructionCostService constructionCostService;
+
     public Optional<ConstructionInstance> getConstructionInstance(final Long id) {
         return constructionInstanceRepository.findById(id);
     }
@@ -23,6 +31,25 @@ public class ConstructionInstanceService {
 
     public ConstructionInstance saveConstructionInstance(ConstructionInstance constructionInstance) {
         return constructionInstanceRepository.save(constructionInstance);
+    }
+
+    public ConstructionInstance updateTavern(long id) {
+        Optional<ConstructionInstance> optionalConstructionInstance = this.getConstructionInstance(id);
+        ConstructionInstance constructionInstance = optionalConstructionInstance.get();  // j'ai mon instance de taverne avec son actual Rank
+
+        Optional<ConstructionCost> optionalConstructionCost = constructionCostService.getConstructionCost(new Long(constructionInstance.getActualRank()));
+        ConstructionCost constructionCost = optionalConstructionCost.get(); // Je prend le cout de construction d'une taverne ou l'id est = a actual Rank de mon instance.
+
+        Inventory inventory = inventoryService.getInventories().iterator().next();  //je prend mon inventaire perso.
+
+        if (inventory.getGold() - constructionCost.getTavern() > 0) {  // Si 100 - 20 > 0 alors...
+            inventory.setGold(inventory.getGold() - constructionCost.getTavern());
+            constructionInstance.setActualRank(constructionInstance.getActualRank() + 1);
+            this.saveConstructionInstance(constructionInstance);
+            return constructionInstance;
+        }else {
+            return constructionInstance;
+        }
     }
 
 }
