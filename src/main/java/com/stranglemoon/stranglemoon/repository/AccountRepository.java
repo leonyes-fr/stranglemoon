8ec812f16517fe15db2dff4268909671c3b576f3
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-27 22:21:03 +0200

    table account

---
+++ src/main/java/com/stranglemoon/stranglemoon/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.stranglemoon.stranglemoon.repository;
+
+import com.stranglemoon.stranglemoon.model.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Long> {
+}
